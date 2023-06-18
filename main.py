acac76b394e5960ecddbb9488e09053c6478acd0
Cer0un0 <wataru.mrt@gmail.com>
2023-06-18 10:51:27 +0900

    memory機能を追加

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from discord import Client, Intents, Message, Thread
 from langchain.agents import ZeroShotAgent, AgentExecutor, load_tools
 from langchain import OpenAI, LLMChain
 from langchain.agents import initialize_agent
-from langchain.agents import AgentType
-
+from langchain.agents import AgentType, Tool
+from langchain.llms import OpenAIChat
+from langchain.chat_models import ChatOpenAI
+# Google Search API
+from langchain.utilities import GoogleSearchAPIWrapper
+# Memory
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 TOKEN = os.environ['TOKEN']  # discord botのtoken
@@ -18,12 +23,29 @@ LIMIT = 200  # openaiにAPIで送る会話の上限
 client: Client = Client(intents=Intents.all())
 
 async def search(search_str: str):
-    search_str += '\n日本語で回答してください'
+#    search_str += '\n日本語で回答してください'
 
-    llm = OpenAI(temperature=0)
+    search = GoogleSearchAPIWrapper()
+    tools = [
+        Tool(
+            name = "Search",
+            func=search.run,
+            description="useful for when you need to answer questions about current events"
+        )
+    ]
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    prefix = os.environ['PREFIX']
+    suffix = os.environ['SUFFIX']
+
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo',temperature=0)
+    # tool, memory, llm を設定して agent を作成
+    agent_chain = initialize_agent(tools, llm, agent="chat-conversational-react-description", verbose=True, memory=memory, prefix=prefix, suffix=suffix)
+    '''
     tools = load_tools(["google-search"], llm=llm)
     agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
     result = agent.run(search_str)
+    '''
+    result = agent_chain.run(input=search_str)
     return result
 
 async def chatgpt(message: Message):
